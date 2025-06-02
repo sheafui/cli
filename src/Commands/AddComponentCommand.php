020a72335e391f36597bde7c4980d731b6472873
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2025-06-02 07:16:13 +0100

    wip

--- src/Commands/AddComponentCommand.php
+++ src/Commands/AddComponentCommand.php
@@ -3,6 +3,8 @@
 namespace Fluxtor\Cli\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
 
 class AddComponentCommand extends Command
 {
@@ -25,6 +27,27 @@ class AddComponentCommand extends Command
      */
     public function handle()
     {
-        $this->info("Adding a fluxtor component");
+        $serverUrl = config('fluxtor.cli.server_url');
+
+        $componentName = $this->argument("name");
+
+        $component = Http::get($serverUrl . '/api/cli/components/' . $componentName);
+        
+        if($component->failed()) {
+            $this->error("Failed: " . $component->reason());
+            return;
+        }
+
+        collect($component['files'])->each(function ($file) {
+            $filePath = $file['path'];
+            if(!file_exists($filePath)) {
+                File::replace($filePath, $file['content']);
+
+                $this->info("File has been created at " . $filePath);
+            }else {
+                dump($filePath . " File exist. should be ovveriding?");
+            }
+        });
+
     }
 }
