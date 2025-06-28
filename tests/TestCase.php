00e114a593738e473c49bc31aec745059c5ee954
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2025-06-28 13:21:14 +0100

    wip

---
+++ tests/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+
+namespace Fluxtor\Cli\Tests;
+
+use Orchestra\Testbench\PHPUnit\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase {}
---
+++ tests/Unit/ListCommandTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fluxtor\Cli\Tests\Unit;
+
+use Fluxtor\Cli\Tests\TestCase;
+
+class ListCommandTest extends TestCase {
+
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
