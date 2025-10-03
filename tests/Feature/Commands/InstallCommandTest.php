44526ee1223bc18888a4b5bcbdba44666cbc5e83
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2025-10-03 22:59:19 +0100

    wip

--- tests/Feature/Commands/InstallCommandTest.php
+++ tests/Feature/Commands/InstallCommandTest.php
@@ -1,13 +1,21 @@
 <?php
 
 
-it("Should be able to install a component", function () {
-    $this->artisan("sheaf:install")
-    ->expectsQuestion("What are the component(s) you would like to install?", "alerts")
-    ->assertExitCode(0);
+it("Should be able to initialize the package", function() {
+    $result = $this->artisan("sheaf:init --skip-prompts"); // exit with code 1 so the test fails
 
-    $this->view('components.ui.alerts.index');
-
-    // expect(resource_path('views/components/ui/button'))->toBeDirectory();
-    // expect(resource_path('views/components/ui/button/index.blade.php'))->toBeFile();
 });
+
+// it("Should be able to install a component", function () {
+
+//     $result = $this->artisan("sheaf:init --skip-prompts")->run();
+
+//     dd($result);
+
+
+//     $this->artisan("sheaf:install alerts --internal-deps --external-deps --force")
+//     ->assertExitCode(0);
+
+//     $this->view('components.ui.alerts.index');
+
+// });
--- tests/Unit/ExampleTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Sheaf\Cli\Tests\Unit;
-
-test('that true is true', function () {
-    expect(true)->toBeTrue();
-});
